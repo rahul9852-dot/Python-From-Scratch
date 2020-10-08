0cd469e9935299d442e37a9aa34e7688b409d411
Rahul Kumar <srayesh5432@gmail.com>
2020-10-08 13:02:32 +0530

    Clearning list

---
+++ List/clear.py
@@ -0,0 +1,10 @@
+# Python program to clear a list
+# Using clear() method
+
+# Creating list
+GEEK=[6,0,4,1]
+print('Geek before clear:',GEEK)
+
+# Clearing list
+GEEK.clear()
+print("GEEK after clear:",GEEK)
